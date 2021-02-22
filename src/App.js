f7421eb548fa56bb3780dbc3d66000b5f2592bbe
JARRAY-YASSINE <62037959+JARRAY-YASSINE@users.noreply.github.com>
2021-02-22 21:57:49 +0200

    first component

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+
+import React from "react";
+import MyFirstComponent from "./component/MyFfirstComponent";
+const App = () => (
+<>
+<MyFirstComponent />
+</>
+);
+export default App;
---
+++ src/component/MyFfirstComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+
+
+
+
+const firstComponent = () => {
+ return <h1> hello from my first component </h1>;
+};
+export default firstComponent;
