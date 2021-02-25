16a1bfc34304648d6e61c00536b1807bd1d19ed7
JARRAY-YASSINE <62037959+JARRAY-YASSINE@users.noreply.github.com>
2021-02-25 16:37:43 +0200

    card for component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import './App.css';
-
+import './assets/style.css';
 import React from "react";
-import MyFirstComponent from "./component/MyFfirstComponent";
-const App = () => (
-<>
-<MyFirstComponent />
-</>
-);
+//import MyFirstComponent from "./component/MyFfirstComponent";
+import Address from "./component/Profile/Address";
+import FullName from './component/Profile/FullName';
+import ProfilePhoto from './component/Profile/ProfilePhoto';
+
+
+function App () {
+    return (
+
+    <div className='App'>
+          {/* <MyFirstComponent /> */}
+    <div className='YassCard'>
+
+        <Address/>
+        <FullName/>
+        <ProfilePhoto/>
+
+    </div>
+    </div>
+)
+}
 export default App;
